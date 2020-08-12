66cc1537836df0829de132315bbc3b2cd80bfb0d
antaramoni <antara.canada@gmail.com>
2020-08-12 02:01:30 -0300

    Indenting done

--- cypress/integration/Autolearning/pageObjects/homePage.js
+++ cypress/integration/Autolearning/pageObjects/homePage.js
@@ -1,64 +1,43 @@
-class homePage
-{
+class homePage {
+  FromField() {
+    return cy.get("");
+  }
 
-    FromField()
-    {
-    
-      return cy.get('');
-    }
-    
-    ToField()
-    {
-    
-      return cy.get('');
-    }
-    
-    DepartField()
-    {
-    
-      return cy.get('');
-    }
-    
-    ReturnField()
-    {
-    
-      return cy.get('');
-    }
-    
-    
-    PassengersField()
-    {
-    return cy.get('')
-    }
-    
-    ClassField()
-    {
-    return cy.get('')
-    }
-    
-    EnterYourEmailAddressTextField()
-    {
-      return cy.get('')
-    }
-    
-    SignUpField()
-    {
-      return cy.get('')
-    }
-    
-    EnterACityTextField()
-    {
-      return cy.get('')
-    }
-    
-    SearchButtonEnterAcity()
-    
-    {
-      return cy.get('')
-    }
-    
+  ToField() {
+    return cy.get("");
+  }
 
+  DepartField() {
+    return cy.get("");
+  }
 
+  ReturnField() {
+    return cy.get("");
+  }
+
+  PassengersField() {
+    return cy.get("");
+  }
+
+  ClassField() {
+    return cy.get("");
+  }
+
+  EnterYourEmailAddressTextField() {
+    return cy.get("");
+  }
+
+  SignUpField() {
+    return cy.get("");
+  }
+
+  EnterACityTextField() {
+    return cy.get("");
+  }
+
+  SearchButtonEnterAcity() {
+    return cy.get("");
+  }
 }
 
 export default homePage;
--- cypress/integration/Autolearning/pageObjects/myAccountPage.js
+++ cypress/integration/Autolearning/pageObjects/myAccountPage.js
@@ -1,35 +1,19 @@
+class myAccountPage {
+  loginEmail() {
+    return cy.get("#account-login-email");
+  }
 
+  loginPass() {
+    return cy.get("#account-login-password");
+  }
 
-class myAccountPage
-{
+  loginSubmit() {
+    return cy.get(".ss-form-row").eq(2);
+  }
 
-
-
-
-loginEmail()
-{
-   return cy.get('#account-login-email')
-}
- 
-loginPass()
-{
-   return cy.get('#account-login-password')
-}
-
-  
-loginSubmit()
-{
-   return cy.get('.ss-form-row').eq(2)
-}
-
-
-
-loginSuccess()
-{
-   return cy.get('.myaccount-trips-tabs');
-}
-
-
+  loginSuccess() {
+    return cy.get(".myaccount-trips-tabs");
+  }
 }
 
 export default myAccountPage;
